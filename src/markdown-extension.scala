fd27f05a3dc4f06785a6fc3cb96e926357217f0b
Mikami Hiroaki <hiroaki8270@gmail.com>
2014-09-24 17:10:48 +0900

    htmlタグの解釈をxmlではなく, 自力で実装することで実現した.

--- src/markdown-extension.scala
+++ src/markdown-extension.scala
@@ -10,36 +10,63 @@ exec scala -nowarn "$0" "$@"
 trait Plugin {
   def prefix: String = ""
   protected[this] def tag: String
-  protected[this] def convertTargetedText(tag: String, text: String): String
+  protected[this] def convertTargetedText(attributes: String, text: String): String
+  // TODO attributesをちゃんとパースして, HashMapとかにしたほうが良い？
   def convert(markdown: String): String = {
-    // 対応するタグを調べて変換する.
-    val startTagExp = "<${tag}(.*)>(.*)".r
-    val endTagExp   = "(.*)</${tag}>(.*)".r
-    
-    def findTag(text: String): String = {
-      text
-    }
-
-    markdown
-
-/*
-    // タグが対応タグであるかどうかを調べる.
-    if(markdown.label == tag) {
-      // 対応するtagである場合
-      // 変換の実行
-      val convertResult = convertTargetedText(markdown)
-
-      if (isAddBackslash) {
-        // 各種特殊文字の変更
-        /* TODO 特殊文字に<, >を入れる場合, htmlのタグすら変換してしまい, おかしなことになる.*/
-        List('\\', '(', ')', '[', ']', '*', '_').foldLeft(convertResult)((text, c) => addBackslash(text, c))
-      } else {
-	convertResult
+    // tagを表す正規表現
+    val beginTag = s"([[^\n]|\n]*)<${tag}([^>])*>([[^\n]|\n]*)".r
+    val endTag = s"([[^\n]|\n]*)</${tag}>([[^\n]|\n]*)".r
+    // Tagを表すデータ構造
+    abstract class Tag
+    case class BeginTag(attributes: String) extends Tag
+    object EndTag extends Tag
+    type Element = Either[Tag, String]
+    // Tagごとに分割する関数
+    def splitByTag(text: String): Seq[Element] = {
+      text match {
+	case beginTag(t1, attr, t2) if attr == null => splitByTag(t1) ++ List(Left(BeginTag("")): Element) ++ splitByTag(t2)
+	case beginTag(t1, attr, t2) if attr != null => splitByTag(t1) ++ List(Left(BeginTag(attr)): Element) ++ splitByTag(t2)
+	case endTag(t1, t2) => splitByTag(t1) ++ List(Left(EndTag): Element) ++ splitByTag(t2)
+	case _ => List(Right(text))
       }
-    } else {
-      markdown
     }
-*/
+
+    val tree = splitByTag(markdown)
+    var text = ""
+    var attr = ""
+    var buffer = ""
+    var depth = 0
+    for(elem <- tree) {
+      elem match {
+	case Right(str) => 
+	  if (depth == 0 ) {
+	    text = text + str
+	  } else {
+	    buffer = buffer + str
+	  }
+	case Left(BeginTag(_attr)) => 
+	  if (depth == 0) {
+	    // 属性の保存
+	    attr = _attr
+	  } else {
+	    // bufferへの追加
+	    buffer = buffer + s"<${tag} ${attr}>"
+	  }
+	  depth = depth + 1
+	case Left(EndTag) => 
+	  depth = depth - 1
+	  if (depth == 0) {
+	    // 文字の変換
+	    text = text + convertTargetedText(attr, buffer)
+	    attr = ""
+	    buffer = ""
+	  } else {
+	    // bufferへの追加
+	    buffer = buffer + s"</${tag}>"
+	  }
+      }
+    }
+    text
   }
 }
 
@@ -51,18 +78,17 @@ object LatexPlugin extends Plugin {
   src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 ></script>"""
   val tag: String = "tex"
-  def convertTargetedText(tag: String, text: String): String = {
+  def convertTargetedText(attributes: String, text: String): String = {
     def addBackslash(text: String, c: Char) = { text.replace(s"$c", s"\\$c") }
     // 各種特殊文字の変更
-    /* TODO 特殊文字に<, >を入れる場合, htmlのタグすら変換してしまい, おかしなことになる.*/
     val textAddedBackslash = List('\\', '(', ')', '[', ']', '*', '_').foldLeft(text)((text, c) => addBackslash(text, c))
 
     if(textAddedBackslash.lines.length == 1) {
       // 1行ならば, \(, \)で囲う
-      s"\\(${textAddedBackslash}\\)"
+      s"\\\\(${textAddedBackslash}\\\\)"
     } else {
       // 1行ならば, \[, \]で囲う
-      s"\\[${textAddedBackslash}\\]"
+      s"\\\\[${textAddedBackslash}\\\\]"
     }
   }
 }
@@ -71,7 +97,7 @@ object LatexPlugin extends Plugin {
 object DotPlugin extends Plugin {
   val tag: String = "dot"
   // TODO engineを取得できるようになると良いかもしれない.
-  def convertTargetedText(tag: String, text: String): String = {
+  def convertTargetedText(attributes: String, text: String): String = {
     // dot言語の取得
     val dot = text
     // svgへの変換
@@ -82,18 +108,18 @@ object DotPlugin extends Plugin {
 // codeプラグイン
 object CodePlugin extends Plugin {
   val tag: String = "code"
-  def convertTargetedText(tag: String, text: String): String = s"<pre><${tag}>${text}</code></pre>"
+  def convertTargetedText(attributes: String, text: String): String = s"<pre><code ${attributes}>${text}</code></pre>"
 }
 val pluginList: List[Plugin] = List(LatexPlugin, DotPlugin, CodePlugin)
 
 // 引数の確認
-if (argv.length == 0) {
+if (args.length == 0) {
   Console.println("#usage markdown-extension <MarkdownFileName>")
-  exit(1)
+  System.exit(1)
 }
 
 // markdownファイルの読み込み.
-val markdownString = (scala.io.Source.fromFile(argv(0))).fold("")((str, line) => s"${str}${line}")
+val markdownString = (scala.io.Source.fromFile(args(0))).fold("")((str, line) => s"${str}${line}")
 
 // prefixをつける
 pluginList foreach ((p: Plugin) => println(p.prefix))
