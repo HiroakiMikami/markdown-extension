a548449a21d8f1ef30824d8e83bf5fd159ea29bc
Mikami Hiroaki <hiroaki8270@gmail.com>
2014-10-04 18:30:35 +0900

    とりあえずpluginはできた. 問題もないんでね.

---
+++ chrome/src/main.js
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////////////
+// (c)2013 casley consulting inc, all right reserved.
+// サンプル
+// author y.Hata 2013/09/20
+///////////////////////////////////////////////////////////
+ 
+document.OnLoad = start();
+//////////////////////////////
+// ページ読込時の処理
+//////////////////////////////
+function start() {
+    var markdown = ""
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function() {
+	var READYSTATE_COMPLETED = 4;
+	var HTTP_STATUS_OK = 200;
+	if( this.readyState == READYSTATE_COMPLETED  && this.status == HTTP_STATUS_OK ) {
+            // レスポンスの表示
+	    document.documentElement.innerHTML = this.responseText
+	} else if ( this.readyState == READYSTATE_COMPLETED && this.status == 0 ) {
+	    // markdownを得る.
+	    markdown = this.responseText;
+	    
+	    // httpServerにPOSTを飛ばす.
+	    xmlHttpRequest.open( 'POST', 'http://localhost:8080/post' );
+	    xmlHttpRequest.send(markdown);
+	} else {
+	    console.log("maybe error")
+	}
+    }
+ 
+    document.charset = "utf-8"
+
+    // http越しにmarkdownを取得(文字コード変換とかがやりやすい.
+    xmlHttpRequest.open('GET', location.href)
+    xmlHttpRequest.send("")
+}
